5ddcc44565f25f4d159fd6a1a96705865678acaa
Mars Ikeda <89821623+mars-ikeda@users.noreply.github.com>
2023-05-08 14:09:01 -0400

    initial commit

---
+++ Initial coding.R
@@ -0,0 +1,8 @@
+Listings_boston.df <- read.csv("/Users/marsikeda/Smith College/SDS 236 Data Journalism/college-home/AirBnB data/Boston/listings.csv")
+nabes_boston.df <- read.csv("/Users/marsikeda/Smith College/SDS 236 Data Journalism/college-home/AirBnB data/Boston/neighbourhoods.csv")
+
+listings_nyc.df <- read.csv("/Users/marsikeda/Smith College/SDS 236 Data Journalism/college-home/AirBnB data/NYC/listings.csv")
+nabes_nyc.df <- read.csv("/Users/marsikeda/Smith College/SDS 236 Data Journalism/college-home/AirBnB data/NYC/neighbourhoods.csv")
+
+listings_RI.df <- read.csv("/Users/marsikeda/Smith College/SDS 236 Data Journalism/college-home/AirBnB data/Rhode Island/listings.csv")
+nabes_RI.df <- read.csv("/Users/marsikeda/Smith College/SDS 236 Data Journalism/college-home/AirBnB data/Rhode Island/neighbourhoods.csv")

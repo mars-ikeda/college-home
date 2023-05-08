b0c9c0d73dd13a23b2c815bcec8008293faafc9e
Mars Ikeda <89821623+mars-ikeda@users.noreply.github.com>
2023-05-08 18:59:37 -0400

    creating dataframes

--- Initial coding.R
+++ Initial coding.R
@@ -1,3 +1,4 @@
+library(tidyverse)
 Listings_boston.df <- read.csv("/Users/marsikeda/Smith College/SDS 236 Data Journalism/college-home/AirBnB data/Boston/listings.csv")
 nabes_boston.df <- read.csv("/Users/marsikeda/Smith College/SDS 236 Data Journalism/college-home/AirBnB data/Boston/neighbourhoods.csv")
 
@@ -6,3 +7,21 @@ nabes_nyc.df <- read.csv("/Users/marsikeda/Smith College/SDS 236 Data Journalism
 
 listings_RI.df <- read.csv("/Users/marsikeda/Smith College/SDS 236 Data Journalism/college-home/AirBnB data/Rhode Island/listings.csv")
 nabes_RI.df <- read.csv("/Users/marsikeda/Smith College/SDS 236 Data Journalism/college-home/AirBnB data/Rhode Island/neighbourhoods.csv")
+
+harvard.df <- Listings_boston.df |>
+  select(id, neighbourhood, room_type, price, minimum_nights, availability_365)
+
+harvard_ab.df <- harvard.df |>
+  filter(neighbourhood == "Allston" | neighbourhood == "Brighton")
+
+columbia.df <- listings_nyc.df |>
+  select(id, neighbourhood, room_type, price, minimum_nights, availability_365)
+
+columbia_mh.df <- columbia.df |>
+  filter(neighbourhood == "Morningside Heights")
+
+brown.df <- listings_RI.df |>
+  select(id, neighbourhood, room_type, price, minimum_nights, availability_365)
+
+brown_wp.df <- brown.df |>
+  filter(neighbourhood == "West Providence")
